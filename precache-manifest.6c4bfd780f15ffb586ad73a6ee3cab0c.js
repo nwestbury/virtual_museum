0b1a5bf550dcc89ec245f9c15dcb019c434d769a
Nico <5westbury5@gmail.com>
2019-03-24 00:36:31 -0400

    Updates

---
+++ precache-manifest.6c4bfd780f15ffb586ad73a6ee3cab0c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "01cdb624d310d592e24ece7a0dcf94b8",
+    "url": "/static/media/jaw.01cdb624.png"
+  },
+  {
+    "revision": "fe0f4355adecaf4e5f99",
+    "url": "/static/css/main.bba7f96e.chunk.css"
+  },
+  {
+    "revision": "afa6b757a6bd185bf8a8",
+    "url": "/static/js/1.afa6b757.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2144f5e0c01906becf4d1caa9e12b86e",
+    "url": "/static/media/waterhome.2144f5e0.mp4"
+  },
+  {
+    "revision": "5d6c7d6645cdfc01f5e8311b91b487cf",
+    "url": "/static/media/geo-timescale.5d6c7d66.gif"
+  },
+  {
+    "revision": "ea39a73496e6148da9f05fda241fea74",
+    "url": "/static/media/fins.ea39a734.png"
+  },
+  {
+    "revision": "bc761cf03730d17b767fb34cc25eafc3",
+    "url": "/static/media/head.bc761cf0.png"
+  },
+  {
+    "revision": "fe0f4355adecaf4e5f99",
+    "url": "/static/js/main.fe0f4355.chunk.js"
+  },
+  {
+    "revision": "191c9329c837ea97f79a381a8cd79141",
+    "url": "/static/media/trunk.191c9329.png"
+  },
+  {
+    "revision": "46b3d91ffaaa7178da3a2b283878a2ac",
+    "url": "/static/media/graph_pie.46b3d91f.jpg"
+  },
+  {
+    "revision": "5ca9fb8e317044d81003d07cece86df1",
+    "url": "/static/media/geoanimation.5ca9fb8e.mov"
+  },
+  {
+    "revision": "4cdda2bb17e3aee22c8e4a82a1cdb610",
+    "url": "/static/media/water.4cdda2bb.mp4"
+  },
+  {
+    "revision": "820e89ec3deb855347f74e55a24429fa",
+    "url": "/static/media/water.820e89ec.jpg"
+  },
+  {
+    "revision": "448adea47979f74d46f058d17453463f",
+    "url": "/static/media/pool_bck.448adea4.png"
+  },
+  {
+    "revision": "8b07c240c35787f9543ebd61fe344248",
+    "url": "/static/media/sand_bck.8b07c240.jpg"
+  },
+  {
+    "revision": "3fec0876ebb267a2c917b90dffb0d1b9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/virtual_museum/precache-manifest.97de9a0e6e35f244910f570a13529c6e.js"
+  "/precache-manifest.6c4bfd780f15ffb586ad73a6ee3cab0c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/virtual_museum/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
